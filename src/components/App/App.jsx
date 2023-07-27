7485a0d8366bf8b573e242f996b06729f483b354
Skeler667 <loloalilqwertykop@gmail.com>
2023-07-27 16:13:56 +0300

    add and connecting redux | 27.07.2023. 16:13,

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,16 +1,24 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import AppRoutes from './../Routes/Routes';
 import Header from './../Header/Header';
 import Sidebar from './../Sidebar/Sidebar';
 import Footer from './../Footer/Footer';
+import { useDispatch } from 'react-redux';
+import { getCategories } from '../../features/categories/categoriesSlice';
 
 const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getCategories());
+  }, [dispatch])
+  
   return ( 
     <div className='app'>
       <Header />
       <div className='container'> 
         <Sidebar />
-        {/* <AppRoutes />       */}
+        <AppRoutes />      
       </div>
     <Footer />
     </div> 
